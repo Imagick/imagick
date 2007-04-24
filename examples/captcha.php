ee32fdd207b4a97007e5ba1b14496572cca16e3e
mkoppanen <mkoppanen@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-04-24 15:38:42 +0000

    Added a simple captcha example.
    git-svn-id: http://svn.php.net/repository/pecl/imagick/trunk@234406 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/captcha.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+	A very primitive captcha implementation
+*/
+
+/* Create Imagick object */
+$Imagick = new Imagick();
+
+/* Create the ImagickPixel object (used to set the background color on image) */
+$bg = new ImagickPixel();
+
+/* Set the pixel color to white */
+$bg->setColor( 'white' );
+
+/* Create a drawing object and set the font size */
+$ImagickDraw = new ImagickDraw();
+$ImagickDraw->setFontSize( 20 );
+
+/* Create the text */
+$alphanum = 'ABXZRMHTL23456789';
+$string = substr( str_shuffle( $alphanum ), 2, 6 );
+
+/* Create new empty image */
+$Imagick->newImage( 85, 30, $bg ); 
+
+/* Write the text on the image */
+$Imagick->annotateImage( $ImagickDraw, 4, 20, 0, $string );
+
+/* Add some swirl */
+$Imagick->swirlImage( 20 );
+
+/* Create a few random lines */
+$ImagickDraw->line( rand( 0, 70 ), rand( 0, 30 ), rand( 0, 70 ), rand( 0, 30 ) );
+$ImagickDraw->line( rand( 0, 70 ), rand( 0, 30 ), rand( 0, 70 ), rand( 0, 30 ) );
+$ImagickDraw->line( rand( 0, 70 ), rand( 0, 30 ), rand( 0, 70 ), rand( 0, 30 ) );
+$ImagickDraw->line( rand( 0, 70 ), rand( 0, 30 ), rand( 0, 70 ), rand( 0, 30 ) );
+$ImagickDraw->line( rand( 0, 70 ), rand( 0, 30 ), rand( 0, 70 ), rand( 0, 30 ) );
+
+/* Draw the ImagickDraw object contents to the image.
+   The text and the lines.
+*/
+$Imagick->drawImage( $ImagickDraw );
+
+/* Give the image a format */
+$Imagick->setImageFormat( 'png' );
+
+/* Send headers and output the image */
+header( "Content-Type: image/{$Imagick->getImageFormat()}" );
+echo $Imagick->getImageBlob( );
+
+?>
