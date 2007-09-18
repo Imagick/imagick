86148421dd1de4e19ab52219b674c7e8edc35c97
mkoppanen <mkoppanen@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-09-18 10:31:16 +0000

    changed the files to reflect new release
    git-svn-id: http://svn.php.net/repository/pecl/imagick/trunk@242645 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_imagick.h
+++ php_imagick.h
@@ -4,7 +4,7 @@
 
 /* Define Extension Properties */
 #define PHP_IMAGICK_EXTNAME    "imagick"
-#define PHP_IMAGICK_EXTVER     "2.0.0-rc2"
+#define PHP_IMAGICK_EXTVER     "2.0.0-rc3"
 
 /* Import configure options
    when building outside of
