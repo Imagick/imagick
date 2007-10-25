51d3378bd9dfc7a4429d038497790700066a5e42
mkoppanen <mkoppanen@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-10-25 10:06:01 +0000

    Fixed the version string Added the license part
    git-svn-id: http://svn.php.net/repository/pecl/imagick/trunk@244798 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_imagick.h
+++ php_imagick.h
@@ -1,10 +1,29 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 5 / Imagick	                                          |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2006-2007 Mikko Koppanen, Scott MacVicar               |
+   | Imagemagick (c) ImageMagick Studio LLC                               |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Mikko Kopppanen <mkoppanen@php.net>                          |
+   |         Scott MacVicar <scottmac@php.net>                            |
+   +----------------------------------------------------------------------+
+*/
 #ifndef PHP_IMAGICK_H
 /* Prevent double inclusion */
 #define PHP_IMAGICK_H
 
 /* Define Extension Properties */
 #define PHP_IMAGICK_EXTNAME    "imagick"
-#define PHP_IMAGICK_EXTVER     "2.1.0RC1"
+#define PHP_IMAGICK_EXTVER     "2.1.0-rc1"
 #define PHP_IMAGICK_EXTNUM     20101
 
 /* Import configure options
