fe7ef88b4c7279872d0d2a163a188b52cddb5ef6
mkoppanen <mkoppanen@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-05-13 22:02:25 +0000

    No idea what was going on but hopefully fix fit now
    git-svn-id: http://svn.php.net/repository/pecl/imagick/trunk@280475 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_imagick_macros.h
+++ php_imagick_macros.h
@@ -355,25 +355,12 @@
 		if ((orig_width <= desired_width) && (orig_height <= desired_height)) { \
 			RETURN_TRUE; \
 		} \
-		if ((orig_width / desired_width) > (orig_height / desired_height)) { \
+		if ((orig_width - desired_width) > (orig_height - desired_height)) { \
 			ratio = (double)orig_width / (double)desired_width; \
 			desired_height = (double)orig_height / ratio; \
-		} else if ((orig_height / desired_height) > (orig_width / desired_width)) { \
+		} else { \
 			ratio = (double)orig_height / (double)desired_height; \
 			desired_width = (double)orig_width / ratio; \
-		} else { \
-			if (orig_height > orig_width) { \
-				desired_width = 0; \
-			} else { \
-				desired_height = 0; \
-			} \
-			if (desired_width <= 0) { \
-				ratio = (double)orig_height / (double)desired_height; \
-				desired_width = orig_width / ratio; \
-			} else if (desired_height <= 0) { \
-				ratio = (double)orig_width / (double)desired_width; \
-				desired_height = orig_height / ratio; \
-			} \
 		} \
 		if (desired_width < 1) { \
 			desired_width = 1; \
