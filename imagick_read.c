d371964a6f012e481faf2594a998775531025dd7
mkoppanen <mkoppanen@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-11-26 01:28:22 +0000

    simplify url detection code
    git-svn-id: http://svn.php.net/repository/pecl/imagick/trunk@291324 c90b9560-bf6c-de11-be94-00142212c4b1

--- imagick_read.c
+++ imagick_read.c
@@ -41,31 +41,11 @@ zend_bool php_imagick_has_page(char *filename, int filename_len TSRMLS_DC) {
 /* {{{ url format? */
 zend_bool php_imagick_is_url(char *filename, int filename_len TSRMLS_DC)
 {
-	HashTable *wrappers;
-	char *stream_protocol;
-	int key_flags, stream_protocol_len = 0;
-	ulong num_key;
-	
-	if ((wrappers = php_stream_get_url_stream_wrappers_hash())) {
-		HashPosition pos;
-    
-		for (zend_hash_internal_pointer_reset_ex(wrappers, &pos);
-			(key_flags = zend_hash_get_current_key_ex(wrappers, &stream_protocol, &stream_protocol_len, &num_key, 0, &pos)) != HASH_KEY_NON_EXISTANT;
-			zend_hash_move_forward_ex(wrappers, &pos)) {
-				
-			if (key_flags == HASH_KEY_IS_STRING) {
-				if (strncmp(filename, stream_protocol, stream_protocol_len - 1) == 0) {
-					if (filename_len > (stream_protocol_len + 1)) {
-						int start_pos = stream_protocol_len - 1;
-						
-						if (filename[start_pos] == ':' && filename[start_pos+1] == '/' && filename[start_pos+2] == '/') {
-							return 1;
-						}
-					}
-				}
-			}	
-		}
-    } 
+	char *path_for_open;
+
+	if (php_stream_locate_url_wrapper(filename, &path_for_open, STREAM_LOCATE_WRAPPERS_ONLY TSRMLS_CC)) {
+		return 1;
+	}
 	return 0;
 }
 /* }}} */
@@ -74,6 +54,13 @@ zend_bool php_imagick_is_url(char *filename, int filename_len TSRMLS_DC)
 		Returns format length if found, -1 on fail */
 int php_imagick_format_indicator(char *filename, int filename_len TSRMLS_DC) 
 {
+#ifdef PHP_WIN32
+	if (IS_ABSOLUTE_PATH(filename)) {
+		if (count_occurences_of(':', filename TSRMLS_CC) == 1) {
+			return -1;
+		}
+	}
+#endif
 	if (count_occurences_of(':', filename TSRMLS_CC) == 0) {
 		return -1;
 	}
