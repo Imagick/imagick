7144db4209553df40d69584860c50dc3d852870f
Remi Collet <fedora@famillecollet.com>
2013-09-23 13:19:52 +0200

    Fix imagick_helpers.c:141:13: warning: unused variable 'ht' [-Wunused-variable]

--- imagick_helpers.c
+++ imagick_helpers.c
@@ -138,7 +138,6 @@ zend_bool php_imagick_validate_map(const char *map TSRMLS_DC)
 double *php_imagick_zval_to_double_array(zval *param_array, long *num_elements TSRMLS_DC)
 {
 	zval **ppzval;
-	HashTable *ht;
 	double *double_array;
 	long elements, i = 0;
 
