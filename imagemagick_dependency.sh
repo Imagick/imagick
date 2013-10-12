6059269c89abb3b6e2aaaa30e391eb830adf9abd
Mikko <mikko@kuut.io>
2013-10-12 16:21:23 +0800

    Added travis stuff

---
+++ imagemagick_dependency.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+imagemagick_fetch_and_build () {
+	local version=$1
+
+    case $version in
+    dev)
+        svn co https://www.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6/ imagemagick-dev
+		cd imagemagick-dev
+        ;;
+    *)
+		wget "https://launchpad.net/imagemagick/main/${version}/+download/ImageMagick-${version}.tar.gz"
+		tar xvfz ImageMagick-${version}.tar.gz
+		cd ImageMagick-*
+        ;;
+    esac
+    ./configure --prefix="${HOME}/imagemagick-${version}" --without-magick-plus-plus --without-perl
+    make -j 8
+    make install
+    cd ..
+}
+imagemagick_fetch_and_build $1
