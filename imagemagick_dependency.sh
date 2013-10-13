d8659f95962fdc1d47a577491d6a38d33750a9ef
Mikko <mikko@kuut.io>
2013-10-13 22:16:08 +0700

    Does disable openmp help here?

--- imagemagick_dependency.sh
+++ imagemagick_dependency.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
 imagemagick_fetch_and_build () {
-	local version=$1
+    local version=$1
 
     case $version in
-    dev)
+    trunk)
+        svn co https://www.imagemagick.org/subversion/ImageMagick/trunk imagemagick-trunk
+        cd imagemagick-trunk
+        ;;
+    stable-dev)
         svn co https://www.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6/ imagemagick-dev
-		cd imagemagick-dev
+        cd imagemagick-dev
         ;;
     *)
-		wget "https://launchpad.net/imagemagick/main/${version}/+download/ImageMagick-${version}.tar.gz"
-		tar xvfz ImageMagick-${version}.tar.gz
-		cd ImageMagick-*
+        wget "https://launchpad.net/imagemagick/main/${version}/+download/ImageMagick-${version}.tar.gz"
+        tar xvfz ImageMagick-${version}.tar.gz
+        cd ImageMagick-*
         ;;
     esac
-    ./configure --prefix="${HOME}/imagemagick-${version}" --without-magick-plus-plus --without-perl
+    ./configure --prefix="${HOME}/imagemagick-${version}" --without-magick-plus-plus --without-perl --disable-openmp
     make -j 8
     make install
     cd ..
