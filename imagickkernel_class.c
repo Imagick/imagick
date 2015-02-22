8e8eff4111382efcfb61504fb04fa7153d3eba4e
Jan-E <github@ehrhardt.nl>
2015-02-23 00:02:40 +0100

    Fix VC11 build in master branch

--- imagickkernel_class.c
+++ imagickkernel_class.c
@@ -199,12 +199,12 @@ PHP_METHOD(imagickkernel, frommatrix)
 	}
 
 	for (row=0 ; row<num_rows ; row++) {
+		zval tmp_zval, *tmp_pzval;
 		if (zend_hash_index_find(Z_ARRVAL_P(kernel_array), row, (void **) &ppzval_outer) != SUCCESS) {
 			php_imagick_throw_exception(IMAGICKKERNEL_CLASS, MATRIX_ERROR_UNEVEN TSRMLS_CC);
 			goto cleanup;
 		}
 
-		zval tmp_zval, *tmp_pzval;
 		column = 0;
 
 		if (Z_TYPE_PP(ppzval_outer) == IS_ARRAY ) {
