48c1a293094190265370862cc6d15e520ba95507
Danack <Danack@basereality.com>
2015-09-27 01:38:51 +0000

    Avoid re-downloading source files when cached version is going to be used anyway.

--- imagemagick_dependency.sh
+++ imagemagick_dependency.sh
@@ -8,24 +8,37 @@ imagemagick_fetch_and_build () {
 
     echo "version is ${version}"
     im_dir=${HOME}/im/imagemagick-${version}
-    cacheable="true"
+
+    case $version in
+        git7)
+            ;;
+        git6)
+            ;;
+        dev)
+            ;;
+        *)
+    # Both individual commits, and tagged versions are cacheable
+        if [ -d "${im_dir}" ]; then
+            echo "Using cached directory ${im_dir}"
+            return  
+        fi
+        ;;
+    esac
+
 
     case $version in
 
     git7)
-        cacheable="false"
         wget -O ImageMagick-7.tar.gz https://github.com/ImageMagick/ImageMagick/archive/master.tar.gz
         tar xvfz ImageMagick-7.tar.gz
         cd ImageMagick-master
         ;;
     git6)
-        cacheable="false"
         wget -O ImageMagick-6.tar.gz https://github.com/ImageMagick/ImageMagick/archive/ImageMagick-6.tar.gz
         tar xvfz ImageMagick-6.tar.gz
         cd ImageMagick-ImageMagick-6
         ;;
     dev)
-        cacheable="false"
         svn co https://www.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6/ imagemagick-dev
         cd imagemagick-dev
         ;;
@@ -54,14 +67,12 @@ imagemagick_fetch_and_build () {
 #ignore compile warnings/errors
 set +e
 
-    if [ "${cacheable}" == "false" ] || [ ! -d "${im_dir}" ]; then
-        ./configure --prefix="${HOME}/im/imagemagick-${version}" --without-magick-plus-plus --without-perl --disable-openmp --with-gvc=no
-        make -j 8
-        make install
-        cd ..
-    else
-        echo "Using cached directory ${im_dir}";
-    fi
+
+    ./configure --prefix="${HOME}/im/imagemagick-${version}" --without-magick-plus-plus --without-perl --disable-openmp --with-gvc=no
+    make -j 8
+    make install
+    cd ..
+
 }
 
 imagemagick_fetch_and_build $1
