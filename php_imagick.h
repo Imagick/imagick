788b9cb752be5c63abeb9b504da2ef9e25fb20ef
Unknown <Danack@basereality.com>
2017-01-22 19:57:15 +0000

    Clarified why version is not stored in file.

--- php_imagick.h
+++ php_imagick.h
@@ -23,6 +23,8 @@
 
 /* Define Extension Properties */
 #define PHP_IMAGICK_EXTNAME    "imagick"
+// The version is deliberately left as 'PACKAGE_VERSION' in source code.
+// It is only replaced with the actual version number that packaged through pecl.php.net
 #define PHP_IMAGICK_VERSION    "@PACKAGE_VERSION@"
 #define PHP_IMAGICK_EXTNUM     30403
 
