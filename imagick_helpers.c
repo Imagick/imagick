ef46023bfba719183a8dcf880c1aa14cf77c006c
Philip Hofstetter <phofstetter@sensational.ch>
2020-08-10 06:55:09 +0200

    stop setting `no_separation` in PHP-8 and on
    this field has been removed for PHP-8

--- imagick_helpers.c
+++ imagick_helpers.c
@@ -113,7 +113,9 @@ MagickBooleanType php_imagick_progress_monitor_callable(const char *text, const 
 #endif
 	fci.param_count = 2;
 	fci.params = zargs;
+#if PHP_VERSION_ID < 80000
 	fci.no_separation = 0;
+#endif
 #if PHP_VERSION_ID < 70100
 	fci.symbol_table = NULL;
 #endif
