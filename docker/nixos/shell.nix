88e99cddf5ba6aeaa04923a77768c0a15d61ba3f
Stéphan Kochen <git@stephank.nl>
2021-05-13 15:03:28 +0200

    Fix fontconfig error in NixOS container

--- docker/nixos/shell.nix
+++ docker/nixos/shell.nix
@@ -5,4 +5,7 @@ with pkgs;
 mkShell {
   nativeBuildInputs = [ php74.unwrapped autoconf pkgconfig re2c ];
   buildInputs = [ imagemagick7 pcre2 ];
+
+  # https://github.com/Imagick/imagick/issues/398
+  FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
 }
