188eedf61f9b730c8ff5e7c91535912735e477fd
Stéphan Kochen <git@stephank.nl>
2021-05-14 17:16:34 +0200

    Fix getConfigureOptions in the testing container

--- docker/nixos/shell.nix
+++ docker/nixos/shell.nix
@@ -8,4 +8,7 @@ mkShell {
 
   # https://github.com/Imagick/imagick/issues/398
   FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
+
+  # https://github.com/Imagick/imagick/issues/399
+  MAGICK_CONFIGURE_PATH = "${imagemagick7.dev}/lib/ImageMagick-7.0.11/config-Q16HDRI";
 }
