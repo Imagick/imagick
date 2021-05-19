8c21ea18d264b579ac8234056c6eda2fafc6ac02
Danack <Danack@basereality.com>
2021-05-19 19:24:49 +0100

    Debugging convert not building.

--- docker/installImageMagick.sh
+++ docker/installImageMagick.sh
@@ -17,23 +17,23 @@ cd /var/app/imagemagick_src/${SRCFILE}
 ./configure --with-quantum-depth=16 \
            --with-magick-plus-plus=no \
            --without-perl \
-           --disable-static \
            --disable-docs \
            --with-jpeg=yes \
            --with-png=yes \
            --with-webp=yes
 
+# --disable-openmp \
+#   --libdir=/usr/local/lib \
 
-./configure --with-quantum-depth=16 \
-           --with-magick-plus-plus=no \
-           --without-perl \
-           --disable-docs \
-           --with-jpeg=yes \
-           --with-png=yes \
-           --with-webp=yes
+# --enable-hdri
+# --libdir=/usr/local/lib
+# /lib/tls/x86_64/
+# /usr/lib/tls
+# --disable-openmp \
 
+ln -s /usr/local/lib/libMagickCore-7.Q16HDRI.so.0 /usr/lib/x86_64-linux-gnu/libMagickCore-7.Q16HDRI.so.0
+ln -s /usr/local/lib/libMagickWand-7.Q16HDRI.so.0 /usr/lib/x86_64-linux-gnu/libMagickWand-7.Q16HDRI.so.0
 
-           # --disable-openmp \
 
 make install -j 4
 
