56f33164749c85eb4b5c5f7dfe271c641760222a
Danack <Danack@basereality.com>
2021-07-08 01:28:00 +0100

    Add note about script.

--- util/calculate_test_jobs.php
+++ util/calculate_test_jobs.php
@@ -1,5 +1,9 @@
 <?php
 
+// From PHP 7.4, run-tests.php can accept a -jx parameter to
+// run tests in parallel. This script just does a version check
+// to see if it can be used.
+
 if (PHP_MAJOR_VERSION === 5) {
     echo "";
     exit();
