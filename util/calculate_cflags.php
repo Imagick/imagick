70f43fd93ebc97cd08509be7e5b60a407013aef9
Danack <Danack@basereality.com>
2021-12-17 01:19:14 +0000

    Update cflags, and fix typo.

--- util/calculate_cflags.php
+++ util/calculate_cflags.php
@@ -1,9 +1,11 @@
 <?php
 
 // Run with something like:
-// export CLFAGS=$(php util/calculate_cflags.php "8.0" "7.0.1-0")
+// export CFLAGS=$(php util/calculate_cflags.php "8.0" "7.0.1-0")
 // where the first number is the PHP version, and the second number
 // is the ImageMagick version number
+
+//export CFLAGS=$(php util/calculate_cflags.php "7.3" "7.0.10-27")
 
 if ($argc !== 3) {
     fwrite(STDERR, "usage php calculate_cflags.php \$PHP_VERSION \$IMAGEMAGICK_VERSION \n");
@@ -35,8 +37,12 @@ if (strpos($IMAGEMAGICK_VERSION, "6.8") === 0) {
     $CFLAGS = array("-Wno-deprecated-declarations");
 }
 
-if (strpos($IMAGEMAGICK_VERSION, "6.9") === 0) {
+if (strpos($IMAGEMAGICK_VERSION, "6.9") === 0 ||
+    strpos($PHP_VERSION, "7") === 0) {
     $CFLAGS[] = "-Wno-unused-parameter";
+}
+
+if (strpos($IMAGEMAGICK_VERSION, "6.9") === 0) {
     $CFLAGS[] = "-Wno-unused-variable";
 }
 
