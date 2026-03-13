f3e7a1ed1af04537ed2849e3ae1b1fa06990e986
Remi Collet <remi@remirepo.net>
2026-03-13 08:29:03 +0100

    Fix version for pie installation

--- php_imagick.h
+++ php_imagick.h
@@ -23,9 +23,7 @@
 
 /* Define Extension Properties */
 #define PHP_IMAGICK_EXTNAME    "imagick"
-// The version is deliberately left as 'PACKAGE_VERSION' in source code.
-// It is only replaced with the actual version number that packaged through pecl.php.net
-#define PHP_IMAGICK_VERSION    "@PACKAGE_VERSION@"
+#define PHP_IMAGICK_VERSION    "3.8.1"
 #define PHP_IMAGICK_EXTNUM     30801
 
 /* Import configure options when building 
